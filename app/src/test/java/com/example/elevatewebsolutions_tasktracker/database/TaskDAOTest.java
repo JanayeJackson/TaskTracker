a778384579fd64dd79620bf80e84708afc66907a
Janaye Jackson <97257796+JanayeJackson@users.noreply.github.com>
2025-08-14 21:06:51 -0700

    Initial setup

---
+++ app/src/test/java/com/example/elevatewebsolutions_tasktracker/database/TaskDAOTest.java
@@ -0,0 +1,41 @@
+package com.example.elevatewebsolutions_tasktracker.database;
+
+import junit.framework.TestCase;
+
+public class TaskDAOTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testInsert() {
+    }
+
+    public void testUpdate() {
+    }
+
+    public void testDelete() {
+    }
+
+    public void testDeleteAllTasks() {
+    }
+
+    public void testGetAllTasks() {
+    }
+
+    public void testGetTasksByUserId() {
+    }
+
+    public void testGetTaskById() {
+    }
+
+    public void testGetTasksByStatus() {
+    }
+
+    public void testSearchTasks() {
+    }
+}
