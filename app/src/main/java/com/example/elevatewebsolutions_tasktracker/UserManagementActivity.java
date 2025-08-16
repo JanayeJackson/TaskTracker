bf893c0660d30989a9958bd659133858ea3015dd
Janaye Jackson <97257796+JanayeJackson@users.noreply.github.com>
2025-08-15 19:17:49 -0700

    Added alert dialog to ensure there is no accidental deletion

--- app/src/main/java/com/example/elevatewebsolutions_tasktracker/UserManagementActivity.java
+++ app/src/main/java/com/example/elevatewebsolutions_tasktracker/UserManagementActivity.java
@@ -1,7 +1,9 @@
 package com.example.elevatewebsolutions_tasktracker;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +16,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.elevatewebsolutions_tasktracker.auth.services.SessionManager;
 import com.example.elevatewebsolutions_tasktracker.database.TaskManagerRepository;
 import com.example.elevatewebsolutions_tasktracker.database.entities.User;
 import com.example.elevatewebsolutions_tasktracker.databinding.ActivityUserManagementBinding;
@@ -25,6 +28,8 @@ public class UserManagementActivity extends AppCompatActivity {
     private ActivityUserManagementBinding binding;
     private TaskManagerRepository repository;
     private String buttonTitle;
+
+    private SessionManager sessionManager;
     
 
     @SuppressLint("SetTextI18n")
@@ -44,19 +49,20 @@ public class UserManagementActivity extends AppCompatActivity {
         }
 
         repository = TaskManagerRepository.getRepository(getApplication());
-         // Default value for userId
 
         if(userId != -1) {
             // If userId is provided, we are in update mode
             assert repository != null;
+            //get user from the repository
             User user = repository.getUserByUserId(userId).getValue();
             assert user != null;
+            //update the UI with user details
             binding.userNameEditText.setText(user.getUsername());
             binding.passwordEditText.setText(user.getPassword());
             binding.usertitleEditText.setText(user.getTitle());
             binding.adminSwitch.setChecked(user.getAdmin());
 
-
+            // Set the button text and click listener for update
             binding.addUserButton.setText("Update User");
             binding.addUserButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -64,7 +70,7 @@ public class UserManagementActivity extends AppCompatActivity {
                     updateUser(user);
                 }
             });
-
+            //set the button text and click listener for delete
             binding.deleteUserButton.setVisibility(View.VISIBLE);
             binding.deleteUserButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -75,6 +81,7 @@ public class UserManagementActivity extends AppCompatActivity {
         } else {
             // If no userId is provided, we are in add mode
             Log.i(TAG, "No User ID provided, adding new user");
+            // Set the button text and click listener for add
             binding.addUserButton.setText("Add User");
             binding.addUserButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -89,17 +96,38 @@ public class UserManagementActivity extends AppCompatActivity {
     }
 
     private void deleteUser(User user) {
-        //Delete the user with the given userId
-        repository.deleteUser(user);
+        //Display a confirmation dialog before deleting the user
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(UserManagementActivity.this);
+        final AlertDialog alertDialog = alertBuilder.create();
+
+        alertBuilder.setMessage("Delete user " + user.getUsername() + "?");
+
+        alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Delete the user with the given userId
+                repository.deleteUser(user);
+            }
+        });
+        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                alertDialog.dismiss();
+            }
+        });
+
+        alertBuilder.create().show();
     }
 
     private void addUser() {
+        // Get the input values from the EditText fields
         String username = binding.userNameEditText.getText().toString().trim();
-        //String title = binding.titleEditText.getText().toString().trim();
+        String title = binding.usertitleEditText.getText().toString().trim();
         String password = binding.passwordEditText.getText().toString().trim();
         String confirmPassword = binding.confirmPasswordEditText.getText().toString().trim();
         boolean isAdmin = binding.adminSwitch.isChecked();
 
+        // Validate the input values
         if(!password.equals(confirmPassword)){
             toastMaker("Passwords do not match");
             binding.confirmPasswordEditText.setError("Passwords do not match");
@@ -111,16 +139,19 @@ public class UserManagementActivity extends AppCompatActivity {
             // Proceed with adding the user
             toastMaker("Adding user: " + username);
             // add the user to the database
-            repository.insertUser(new User(username, password, " ", isAdmin));
+            repository.insertUser(new User(username, password, title, isAdmin));
         }
     }
 
     private void updateUser(User user) {
+        // Get the input values from the EditText fields
         String username = binding.userNameEditText.getText().toString().trim();
+        String title = binding.usertitleEditText.getText().toString().trim();
         String password = binding.passwordEditText.getText().toString().trim();
         String confirmPassword = binding.confirmPasswordEditText.getText().toString().trim();
         boolean admin = binding.adminSwitch.isChecked();
 
+        // Validate the input values
         if(!password.equals(confirmPassword)){
             toastMaker("Passwords do not match");
             binding.confirmPasswordEditText.setError("Passwords do not match");
@@ -135,7 +166,7 @@ public class UserManagementActivity extends AppCompatActivity {
             assert user != null;
             user.setUsername(username);
             user.setPassword(password);
-            user.setTitle("");
+            user.setTitle(title);
             user.setAdmin(admin);
             repository.updateUser(user);
         }
